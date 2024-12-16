710d5df1bc591d711b8eca5827e30923b5e07844
arielhc378 <ariel_h_c@hotmail.com>
2024-12-15 21:01:02 -0300

    kj

---
+++ carrito.js
@@ -0,0 +1,9 @@
+// Función para abrir el modal
+function abrirModal() {
+    document.getElementById("miModal").style.display = "flex"; // Muestra el modal
+}
+
+// Función para cerrar el modal
+function cerrarModal() {
+    document.getElementById("miModal").style.display = "none"; // Oculta el modal
+}
