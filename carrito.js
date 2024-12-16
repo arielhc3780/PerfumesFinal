8aee086cc2fbf62c643d4894e473837738a4526d
arielhc378 <ariel_h_c@hotmail.com>
2024-12-15 23:17:41 -0300

    2e32

--- carrito.js
+++
@@ -1,9 +0,0 @@
-// Función para abrir el modal
-function abrirModal() {
-    document.getElementById("miModal").style.display = "flex"; // Muestra el modal
-}
-
-// Función para cerrar el modal
-function cerrarModal() {
-    document.getElementById("miModal").style.display = "none"; // Oculta el modal
-}
