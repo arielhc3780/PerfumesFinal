35e44e54b764adf07e61f70a732f098e9f552566
arielhc378 <ariel_h_c@hotmail.com>
2024-12-21 09:11:05 -0300

    casi

--- scriptproductos.js
+++ scriptproductos.js
@@ -1,35 +1,5 @@
 const productos = [
     {
-        nombre: "Armani",
-        descripcion: "Tamaño",
-        imagen: "IMG/armani.png",
-        valor: 100000,
-    },
-    {
-        nombre: "Savage",
-        descripcion: "Tamaño",
-        imagen: "IMG/savage.png",
-        valor: 200000,
-    },
-    {
-        nombre: "Good",
-        descripcion: "Tamaño",
-        imagen: "IMG/good.png",
-        valor: 150000,
-    },
-    {
-        nombre: "La Vie",
-        descripcion: "Tamaño",
-        imagen: "IMG/lavie.png",
-        valor: 250000,
-    },
-    {
-        nombre: "Armani",
-        descripcion: "Tamaño",
-        imagen: "IMG/armani.png",
-        valor: 100000,
-    },
-    {
         nombre: "Savage",
         descripcion: "perfume Savage",
         imagen: "IMG/savage.png",
@@ -94,6 +64,12 @@ const productos = [
         descripcion: "perfume La vie",
         imagen: "IMG/lavie.png",
         valor: 250000,
+    },
+    {
+        nombre: "Armani",
+        descripcion: "perfume Armani",
+        imagen: "IMG/armani.png",
+        valor: 100000,
     },
 ]
 let productosHTML = "";
@@ -102,7 +78,7 @@ for(let indice = 0; indice < productos.length; indice++){
         <div class="card-${indice + 1}">
             <div class="contenido-tarjeta">
                 <h3>${productos[indice].nombre}</h3>
-                <img src=${productos[indice].imagen} alt="savage">
+                <img src=${productos[indice].imagen} alt="${productos[indice].nombre}">
                 <p>${productos[indice].descripcion}</p>
             </div>
             <div class="pie-tarjeta">
