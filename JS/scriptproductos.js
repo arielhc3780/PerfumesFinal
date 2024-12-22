1708fb387038d5fe4897d0c70c740e29740987c5
arielhc378 <ariel_h_c@hotmail.com>
2024-12-22 10:41:12 -0300

    pppp

--- JS/scriptproductos.js
+++ JS/scriptproductos.js
@@ -1,99 +1,145 @@
-//creamos un array de objetos con los productos que se van a mostrar en la página
-const productos = [
-    {
-        nombre: "Savage",
-        descripcion: "perfume Savage",
-        imagen: "IMG/savage.png",
-        valor: 200000,
-    },
-    {
-        nombre: "Good",
-        descripcion: "perfume good",
-        imagen: "IMG/good.png",
-        valor: 150000,
-    },
-    {
-        nombre: "La Vie",
-        descripcion: "perfume La vie",
-        imagen: "IMG/lavie.png",
-        valor: 250000,
-    },
-    {
-        nombre: "Armani",
-        descripcion: "perfume Armani",
-        imagen: "IMG/armani.png",
-        valor: 100000,
-    },
-    {
-        nombre: "Savage",
-        descripcion: "perfume Savage",
-        imagen: "IMG/savage.png",
-        valor: 200000,
-    },
-    {
-        nombre: "Good",
-        descripcion: "perfume good",
-        imagen: "IMG/good.png",
-        valor: 150000,
-    },
-    {
-        nombre: "La Vie",
-        descripcion: "perfume La vie",
-        imagen: "IMG/lavie.png",
-        valor: 250000,
-    },
-    {
-        nombre: "Armani",
-        descripcion: "perfume Armani",
-        imagen: "IMG/armani.png",
-        valor: 100000,
-    },
-    {
-        nombre: "Savage",
-        descripcion: "perfume Savage",
-        imagen: "IMG/savage.png",
-        valor: 200000,
-    },
-    {
-        nombre: "Good",
-        descripcion: "perfume good",
-        imagen: "IMG/good.png",
-        valor: 150000,
-    },
-    {
-        nombre: "La Vie",
-        descripcion: "perfume La vie",
-        imagen: "IMG/lavie.png",
-        valor: 250000,
-    },
-    {
-        nombre: "Armani",
-        descripcion: "perfume Armani",
-        imagen: "IMG/armani.png",
-        valor: 100000,
-    },
-]
+// //creamos un array de objetos con los productos que se van a mostrar en la página
+// const productos = [
+//     {
+//         nombre: "Savage",
+//         descripcion: "perfume Savage",
+//         imagen: "IMG/savage.png",
+//         valor: 200000,
+//     },
+//     {
+//         nombre: "Good",
+//         descripcion: "perfume good",
+//         imagen: "IMG/good.png",
+//         valor: 150000,
+//     },
+//     {
+//         nombre: "La Vie",
+//         descripcion: "perfume La vie",
+//         imagen: "IMG/lavie.png",
+//         valor: 250000,
+//     },
+//     {
+//         nombre: "Armani",
+//         descripcion: "perfume Armani",
+//         imagen: "IMG/armani.png",
+//         valor: 100000,
+//     },
+//     {
+//         nombre: "Savage",
+//         descripcion: "perfume Savage",
+//         imagen: "IMG/savage.png",
+//         valor: 200000,
+//     },
+//     {
+//         nombre: "Good",
+//         descripcion: "perfume good",
+//         imagen: "IMG/good.png",
+//         valor: 150000,
+//     },
+//     {
+//         nombre: "La Vie",
+//         descripcion: "perfume La vie",
+//         imagen: "IMG/lavie.png",
+//         valor: 250000,
+//     },
+//     {
+//         nombre: "Armani",
+//         descripcion: "perfume Armani",
+//         imagen: "IMG/armani.png",
+//         valor: 100000,
+//     },
+//     {
+//         nombre: "Savage",
+//         descripcion: "perfume Savage",
+//         imagen: "IMG/savage.png",
+//         valor: 200000,
+//     },
+//     {
+//         nombre: "Good",
+//         descripcion: "perfume good",
+//         imagen: "IMG/good.png",
+//         valor: 150000,
+//     },
+//     {
+//         nombre: "La Vie",
+//         descripcion: "perfume La vie",
+//         imagen: "IMG/lavie.png",
+//         valor: 250000,
+//     },
+//     {
+//         nombre: "Armani",
+//         descripcion: "perfume Armani",
+//         imagen: "IMG/armani.png",
+//         valor: 100000,
+//     },
+// ]
 //creamos un string vacío para almacenar el HTML que se va a generar
-let productosHTML = "";
-for(let indice = 0; indice < productos.length; indice++){
-    productosHTML += `
-        <div class="card-${indice + 1}">
+// let productosHTML = "";
+// for(let indice = 0; indice < productos.length; indice++){
+//     productosHTML += `
+//         <div class="card-${indice + 1}">
+//             <div class="contenido-tarjeta">
+//                 <h3>${productos[indice].nombre}</h3>
+//                 <img src=${productos[indice].imagen} alt="${productos[indice].nombre}">
+//                 <p>${productos[indice].descripcion}</p>
+//             </div>
+//             <div class="pie-tarjeta">
+//                 <div>    
+//                     <p>Precio: $${productos[indice].valor}</p>
+//                 </div>
+//                 <div>
+//                     <input class="btnAgregarCarrito" type="button" value="Agregar al carrito">
+//                 </div>
+//             </div>
+//         </div>
+//     `
+// }
+// //seleccionamos el contenedor de los cursos y le asignamos el HTML generado
+// const contenedorCursos = document.getElementById("productos");
+// contenedorCursos.innerHTML = productosHTML;
+const productos = [
+    { nombre: "Savage", descripcion: "150ML", imagen: "IMG/savage.png", valor: 200000, disponible: true },
+    { nombre: "Good", descripcion: "100ML", imagen: "IMG/good.png", valor: 150000, disponible: true },
+    { nombre: "La Vie", descripcion: "125ML", imagen: "IMG/lavie.png", valor: 130000, disponible: true },
+    { nombre: "Armani", descripcion: "100ML", imagen: "IMG/armani.png", valor: 100000, disponible: true },
+    { nombre: "Very irresitible", descripcion: "125ML", imagen: "IMG/very-irresistible.jpg", valor: 50000, disponible: true },
+    { nombre: "cheekyl", descripcion: "100ML", imagen: "IMG/cheeky.webp", valor: 50000, disponible: false },
+    { nombre: "Allure", descripcion: "125ML", imagen: "IMG/allure-homme-sport.jpg", valor: 80000, disponible: true },
+    { nombre: "Scandal", descripcion: "100ML", imagen: "IMG/scandal-pour-homme.jpg", valor: 180000, disponible: true },
+    { nombre: "Coco", descripcion: "125ML", imagen: "IMG/coco-mademoiselle.jpg", valor: 230000, disponible: true },
+
+];
+
+
+function generarHTMLProductos(productos) {
+    return productos.map((producto) => `
+        <div class="card ${producto.disponible ? '' : 'agotado'}">
             <div class="contenido-tarjeta">
-                <h3>${productos[indice].nombre}</h3>
-                <img src=${productos[indice].imagen} alt="${productos[indice].nombre}">
-                <p>${productos[indice].descripcion}</p>
+                <h3>${producto.nombre}</h3>
+                <img src="${producto.imagen}" alt="${producto.nombre}">
+                <p>${producto.descripcion}</p>
             </div>
             <div class="pie-tarjeta">
                 <div>    
-                    <p>Precio: $${productos[indice].valor}</p>
+                    <p>Precio: $${producto.valor.toLocaleString()}</p>
+                </div>
                 </div>
                 <div>
-                    <input class="btnAgregarCarrito" type="button" value="Agregar al carrito">
+                    <input 
+                        class="btnAgregarCarrito" 
+                        type="button" 
+                        value="${producto.disponible ? 'Agregar al carrito' : 'Agotado'}" 
+                        ${producto.disponible ? '' : 'disabled'}
+                    >
                 </div>
-            </div>
         </div>
-    `
+    `).join('');
 }
-//seleccionamos el contenedor de los cursos y le asignamos el HTML generado
+
+// Seleccionar contenedor y asignar HTML generado
 const contenedorCursos = document.getElementById("productos");
-contenedorCursos.innerHTML = productosHTML;
+if (contenedorCursos) {
+    contenedorCursos.innerHTML = generarHTMLProductos(productos);
+} else {
+    console.error('El contenedor "productos" no fue encontrado en el DOM.');
+}
